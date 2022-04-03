9bd381cacac5ec690ccdeb79f556ace86ceb8512
GilangBayuG <gilangbayu48@gmail.com>
2022-04-03 23:27:06 +0700

    Modul 4

---
+++ Modul 4/Round Key.py
@@ -0,0 +1,30 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import operator
+state = [
+    [206, 243, 61, 34],
+    [171, 11, 93, 31],
+    [16, 200, 91, 108],
+    [150, 3, 194, 51],
+]
+
+round_key = [
+    [173, 129, 68, 82],
+    [223, 100, 38, 109],
+    [32, 189, 53, 8],
+    [253, 48, 187, 78],
+]
+
+def matrix(matrix):
+    return [print(chr(j), end="") for sub in matrix for j in sub]
+
+def add_round_key(s, k):
+    result = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+    for i in range(len(s)):
+        for j in range(len(s)):
+            result[i][j] = operator.xor(s[i][j], k[i][j])
+
+    return result
+
+
+print(matrix(add_round_key(state, round_key)))
---
+++ Modul 4/Structure.py
@@ -0,0 +1,19 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+def bytes2matrix(text):
+    """ Converts a 16-byte array into a 4x4 matrix.  """
+    return [list(text[i:i+4]) for i in range(0, len(text), 4)]
+
+def matrix2bytes(matrix):
+    """ Converts a 4x4 matrix into a 16-byte array.  """
+    return [print(chr(j), end="") for sub in matrix for j in sub]
+
+matrix = [
+    [99, 114, 121, 112],
+    [116, 111, 123, 105],
+    [110, 109, 97, 116],
+    [114, 105, 120, 125],
+]
+
+print(matrix2bytes(matrix))
+SyntaxError: invalid syntax
